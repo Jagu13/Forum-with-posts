221d06adef454c9b8909f34f3121885c428685df
Jagoda <jagoda.kwiatkowska39@gmail.com>
2024-02-23 15:41:49 +0100

    Initialize project

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+let posts = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.render("tmp.ejs");
+});
+
+app.post('/submit', (req, res) => {
+    const newPost = {
+        nick: req.body["name"],
+        post: req.body["post"],
+    };
+    posts.push(newPost);
+    res.render("tmp.ejs", newPost);
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
