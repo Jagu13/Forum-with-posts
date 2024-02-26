4eb999c47b2b343e680aca23bf0247228942c137
Jagoda <jagoda.kwiatkowska39@gmail.com>
2024-02-26 23:40:03 +0100

    Adding logic into Bootstrap template v1

--- index.js
+++ index.js
@@ -21,6 +21,20 @@ app.post('/submit', (req, res) => {
     res.render("tmp.ejs", { posts: posts });
 })
 
+
+app.get('/2', (req, res) => {
+    res.render("bs4_beta_user_profile_info_with_posts.ejs", { posts: posts });
+});
+
+app.post('/submit2', (req, res) => {
+    const newPost = {
+        nick: req.body["name"],
+        post: req.body["post"],
+    };
+    posts.push(newPost);
+    res.render("bs4_beta_user_profile_info_with_posts.ejs", { posts: posts });
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
